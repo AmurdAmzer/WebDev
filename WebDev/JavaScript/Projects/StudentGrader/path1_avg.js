8b31d4ce3510d265970ac6fbe48dd0356ef1db4a
AmurdAmzer <amurdamzer@gmail.com>
2025-11-19 18:33:19 -0500

    Week 1 Path 1

--- WebDev/JavaScript/Projects/StudentGrader/path1_avg.js
+++ WebDev/JavaScript/Projects/StudentGrader/path1_avg.js
@@ -16,9 +16,9 @@ function calculateAverage(num1, num2, num3) {
     console.log("Button clicked!");
 
     // Get values from input fields
-    const score1 = number(document.getElementById('num1').value)
-    const score2 = number(document.getElementById('num2').value)
-    const score3 = number(document.getElementById('num3').value)
+    const score1 = Number(document.getElementById('num1').value)
+    const score2 = Number(document.getElementById('num2').value)
+    const score3 = Number(document.getElementById('num3').value)
 
     alert("Your average score is: " + average);
 
