470c5f13c91c3d68a90a8780b5f4fb8342d5af03
AmurdAmzer <amurdamzer@gmail.com>
2025-11-20 11:49:47 -0500

    Week 1 Path 1 mini 2

---
+++ WebDev/JavaScript/Projects/StudentGrader/path1_mini2_Array.js
@@ -0,0 +1,17 @@
+
+function calculateAverage(scores){
+// TODO: Calculate the average of all numbers in the array
+const scores = [];
+let sum = 0;
+for (let i=0; i<scores.length; i++){
+    sum = sum + scores[i];
+
+}
+
+const average = sum/scores.length;
+return average;
+}
+
+// Test it
+console.log(calculateAverage([80, 90, 85])); // Should print 85
+
