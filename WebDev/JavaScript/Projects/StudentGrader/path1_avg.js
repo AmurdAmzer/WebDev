3a73968c64e7d3946ec705662c77dc0eef91d090
AmurdAmzer <amurdamzer@gmail.com>
2025-11-20 09:25:02 -0500

    Week 1 Path 1 mini 2

--- WebDev/JavaScript/Projects/StudentGrader/path1_avg.js
+++ WebDev/JavaScript/Projects/StudentGrader/path1_avg.js
@@ -26,4 +26,10 @@ function calculateAverage(num1, num2, num3) {
     // Show result in alert
     alert("Your average score is: " + average);
 
+    // NB: I can also do alert("Your average score is: " + calculateAverage(score1, score2, score3));
+    // however, I decided to store it in a variable average so I can reuse average anytime I want to call the calculateAverage function.
+
+    //Also, score1, score2, and score3 and the actual numbers inputted by the user in the input fields. 
+
+    // number converts the input to number type since input values are strings by default, and store them in score1, score2, and score3 variables.
   });
