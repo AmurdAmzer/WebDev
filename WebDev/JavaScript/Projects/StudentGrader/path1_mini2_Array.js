4479f0fbe481bb42ec8b35870b9019937557f2f0
AmurdAmzer <amurdamzer@gmail.com>
2025-11-20 13:38:04 -0500

    Week 1 Path 1 mini 2

--- WebDev/JavaScript/Projects/StudentGrader/path1_mini2_Array.js
+++ WebDev/JavaScript/Projects/StudentGrader/path1_mini2_Array.js
@@ -16,8 +16,23 @@ return average;
 console.log(calculateAverage([80, 90, 85])); // Should print 85
 
 const addScoreButton = document.getElementById('addScoreBtn');
-const averageButton = documnet.getElementById('averageBtn');
+const averageButton = document.getElementById('averageBtn');
 
 addScoreButton.addEventListener('click', function(e){
 
 })
+
+averageButton.addEventListener('click', function(){
+    
+    // TODO: Get all scores from the scores array
+
+    // Console log to verify button click
+    console.log("Average Button Clicked!");
+
+    const scores = Number(document.getElementById('score').value);
+
+    const average = calculateAverage(scores);
+
+    alert("Your average score is: " + average);
+
+});
